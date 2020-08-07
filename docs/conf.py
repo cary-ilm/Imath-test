c6851d7a48cec7b35e93e78edb05522327ca18dc
Cary Phillips <cary@ilm.com>
2020-08-07 13:36:39 -0700

    os
    Signed-off-by: Cary Phillips <cary@ilm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 # hack for readthedocs to cause it to run doxygen first
 # https://github.com/rtfd/readthedocs.org/issues/388
